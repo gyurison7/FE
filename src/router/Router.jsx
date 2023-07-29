aaf0f4ac87f0e97f81f609f0be03a4575215e772
mmms2000 <moseskmcmmms@gmail.com>
2023-07-29 13:36:51 +0900

    init: 폴더구조 생성

---
+++ src/router/Router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/main";
+import Login from "../pages/login";
+import Signup from "../pages/singup";
+import Write from "../pages/write";
+
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
