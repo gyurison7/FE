bd448355d2cd8f25b7dc9a1508936ab7c3efe7e7
mmms2000 <moseskmcmmms@gmail.com>
2023-08-07 15:29:32 +0900

    feat: layout init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Layout from "./layout/Layout.js";
 import Router from "./router/Router.jsx";
 
 
 function App() {
   return (
     <>
+    <Layout>
       <Router/>
+    </Layout>
     </>
   );
 }
---
+++ src/layout/Layout.js
@@ -0,0 +1,10 @@
+import { styled } from "styled-components"
+
+const Layout = styled.div`
+    background-color: aqua;
+    max-width: 428px;
+    height: 100vh;
+    margin: 0 auto;
+`
+
+export default Layout
