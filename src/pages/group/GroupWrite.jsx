f42a010b101534a1c9f8c5b7d6a58087f2a57965
mmms2000 <moseskmcmmms@gmail.com>
2023-08-08 12:27:41 +0900

    feat: 그룹 인풋 핸들러들 작성

--- src/pages/group/GroupWrite.jsx
+++ src/pages/group/GroupWrite.jsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 
 function GroupWrite() {
+  const [groupName, setGroupName] = useState('');
+  // const [thumbnailUrl, setThumbnailUrl] = useState("");
+  const [place, setPlace] = useState('');
+  const [places, setPlaces] = useState([]);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [participants, setParticipant] = useState([]);
 
+  console.log('-------------------')
+  console.log('groupName', groupName);
+  console.log('place', places);
+  console.log('startDate', startDate);
+  console.log('endDate', endDate);
+  console.log('participants', participants);
 
+  
+  const groupNameHandler = (e)=>{
+    setGroupName(e.target.value)
+  }
 
+  const placeHandler = (e) =>{
+    setPlace(e.target.value)
+  }
 
+  const placeButtonHandler = ()=>{
+    const newPlaces = place;
+    setPlaces(prevPlaces =>  [...prevPlaces, newPlaces]);
+    setPlace('')
+  }
 
+  const startDateHandler = (e)=>{
+    setStartDate(e.target.value)
+  }
 
+  const endDateHandler = (e)=>{
+    setEndDate(e.target.value)
+  }
+
+  const participantsHandler = (e)=>{
+    setParticipant(e.target.value)
+  }
+
+  const deletePlaceHandler = (indexToDelete) => {
+    setPlaces(prevPlaces => prevPlaces.filter((_, index) => index !== indexToDelete));
+}
 
   const navigate = useNavigate();
   const backButtonHandler = ()=>{
@@ -31,23 +70,53 @@ function GroupWrite() {
 
             <StWriteBody>
                 <div>
-                    <input />
+                    <input 
+                    type='text' 
+                    value={groupName} 
+                    placeholder='그룹 이름을 입력해주세요'
+                    onChange={groupNameHandler}/>
                 </div>
                     <button>image</button>
                 <div>
                     함께한 추억 장소
-                    <input placeholder='지역' />
+                    <input 
+                    placeholder='장소' 
+                    value={place}
+                    onChange={placeHandler}
+                    />
+                    <button onClick={placeButtonHandler}> 추가</button>
                 </div>
+                <ul>
+        {places.map((place, index) => (
+            <li key={index}>
+                {place} 
+                <button onClick={() => deletePlaceHandler(index)}>삭제</button>
+            </li>
+        ))}
+    </ul>
                 <div>
                     함께한 추억 기간
-                    <input type="date" placeholder='2023-08-02' /> ~ 
-                    <input type="date" placeholder='2023-08-03' />
+                    <input 
+                    type="date"
+                     placeholder='2023-08-02' 
+                     value={startDate}
+                     onChange={startDateHandler}
+                     /> ~ 
+                    <input 
+                    type="date"
+                     placeholder='2023-08-03'
+                     value={endDate}
+                     onChange={endDateHandler} />
                 </div>
                 <div>
                     함께한 친구들
-                    <input placeholder='친구 아이디' />
+                    <input 
+                    placeholder='친구 아이디' 
+                    value={participants}
+                    onChange={participantsHandler}/>
                 </div>
             </StWriteBody>
+        
         </>
     )
 }
