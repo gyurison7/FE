17751b427818bf42f6609bf2d2f9f38a506ba578
mmms2000 <moseskmcmmms@gmail.com>
2023-08-09 01:21:06 +0900

    fix: layoutbackground to aqua

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
 const Layout = styled.div`
-  background-color:#5873FE;
+  background-color: aqua;
   max-width: 428px;
   height: 100vh;
   margin: 0 auto;
