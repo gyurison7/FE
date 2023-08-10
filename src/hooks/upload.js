87517d1c3c8c65b3223e605505a5194e71ec9fb7
metablog <khu10735@naver.com>
2023-08-11 02:23:53 +0900

    feat: postWrite 파일구현

--- src/hooks/upload.js
+++ src/hooks/upload.js
@@ -1,8 +1,9 @@
 export async function uploadImage(file) {
   const data = new FormData();
   data.append('file', file);
-  data.append('upload_preset', process.env.REACT_CLOUD_API_NAME);
-  return fetch(process.env.REACT_CLOUD_API_KIY, {
+  data.append('upload_preset', process.env.REACT_APP_CLOUD_API_NAME);
+
+  return fetch(process.env.REACT_APP_CLOUD_API_KEY, {
     method: 'POST',
     body: data,
   })
--- src/pages/post/PostWrite.jsx
+++ src/pages/post/PostWrite.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { uploadImage } from '../../hooks/upload.js';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 function PostWrite() {
+  const { id } = useParams();
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState({});
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    uploadImage(file).then((url) => {
+      const config = {
+        imageUrl: url,
+        title: title,
+      };
+      axios.post(`https://honeyitem.shop/api/group/${id}/memory`, config, {
+        withCredentials: true,
+      });
+    });
+  };
+
   return (
-    <div>PostWrite</div>
-  )
+    <form onSubmit={submitHandler}>
+      <button type="submit">게시하기</button>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input type="file" onChange={handleChange} />
+    </form>
+  );
 }
 
-export default PostWrite
+export default PostWrite;
