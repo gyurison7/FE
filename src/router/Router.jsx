18735c0c8e52ff479a4c298cb894c6eda78f26b6
metablog <khu10735@naver.com>
2023-08-14 14:21:51 +0900

    remove: footer 테스트  페이지지

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -10,6 +10,7 @@ import PostMain from '../pages/post/PostMain.jsx';
 import PostWrite from '../pages/post/PostWrite.jsx';
 import KakaoLoginRedirect from '../pages/kakao-login/KakaoLoginRedirect.jsx';
 import PasswordChange from '../pages/mypage/PasswordChange.jsx';
+import Post from '../pages/post/Post.jsx';
 
 const Router = () => {
   return (
@@ -26,6 +27,7 @@ const Router = () => {
         <Route path='/postwrite/:id' element={<PostWrite />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/pwchange' element={<PasswordChange />} />
+        <Route path='/post' element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
