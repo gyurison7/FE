f6ad41ae1964d39c95e2af12dbe51593f8a4a75e
mmms2000 <moseskmcmmms@gmail.com>
2023-08-21 01:21:27 +0900

    feat: datemodal 구현

--- src/pages/group/GroupEdit.jsx
+++ src/pages/group/GroupEdit.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { styled } from 'styled-components';
 import api from '../../api/index.jsx';
 import { uploadImage } from '../../api/uploadImage.js';
 import WriteImageUpload from '../../components/common/input/WriteImageUpload.jsx';
-import { DatePicker, Space } from 'antd';
 import Input from '../../components/common/input/Input.jsx';
-import moment from 'moment';
 import FriendSearchModal from '../../components/common/modal/NicknameModal.jsx';
 import IconComponents from '../../components/common/iconComponent/IconComponents.jsx';
-
+import {
+  BackButton,
+  DateInput,
+  DivHeaderText,
+  Form,
+  FriendContentWrap,
+  FriendSearchButton,
+  FriendSearchImage,
+  FriendSearchText,
+  GroupWriteInput,
+  ImageInput,
+  PlaceAddButton,
+  PlaceContainer,
+  PlaceInputWrapper,
+  PlaceRemoveButton,
+  PlaceResult,
+  ProfileImage,
+  SelectFrindRemover,
+  SelectFrindWrapper,
+  StDateWrapper,
+  SubmitButton,
+  ThumbedImage,
+  ThumbnailWrapper,
+  WriteBody,
+  WriteHeader,
+  WriteImageWrapper,
+} from './styleContainer.js';
+import DatePicker from '../../components/common/modal/DatePicker.jsx';
 
 function GroupWrite() {
-
-    const { id } = useParams();
-    const navigate = useNavigate();
-    useEffect(() => {
-        console.log(id);
-        api.get(`group/${id}`, { withCredentials: true }).then((res) => {
-            console.log(res.data);
-            setGroupName(res.data.groupName);
-            setStartDate(res.data.startDate);
-            setEndDate(res.data.endDate);
-            setSelectedFriends(res.data.participants);
-            setThumbnailUrl(res.data.thumbnailUrl);
-            if(res.data.place) {
-                const placesArray = JSON.parse(res.data.place);
-                setPlaces(placesArray); 
-            }
-        });
-    }, []);
-    
-
-
+  const { id } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log(id);
+    api.get(`group/${id}`, { withCredentials: true }).then((res) => {
+      console.log(res.data);
+      setGroupName(res.data.groupName);
+      setStartDate(res.data.startDate);
+      setEndDate(res.data.endDate);
+      setSelectedFriends(res.data.participants);
+      setThumbnailUrl(res.data.thumbnailUrl);
+      if (res.data.place) {
+        const placesArray = JSON.parse(res.data.place);
+        setPlaces(placesArray);
+      }
+    });
+  }, []);
 
   const [groupName, setGroupName] = useState('');
   const [thumbnailUrl, setThumbnailUrl] = useState('');
@@ -43,16 +63,12 @@ function GroupWrite() {
   const [participants, setParticipant] = useState([]);
   const [searchResult, setSearchResult] = useState([]);
   const [selectedFriends, setSelectedFriends] = useState([]);
-  
+
   const [chosenFile, setChosenFile] = useState(null);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isDateModal, setDateModal] = useState(false);
 
-  console.log(places)
-  const { RangePicker } = DatePicker;
-  const onChange = (value, dateString) => {
-    setStartDate(dateString[0]);
-    setEndDate(dateString[1]);
-  };
+  console.log(places);
 
   console.log('starDate', startDate);
   console.log('endDate', endDate);
@@ -182,18 +198,13 @@ function GroupWrite() {
     return selectedFriends.some((friend) => friend.loginId === loginId);
   };
 
-  
-
   return (
     <>
       <Form onSubmit={submitHandler} onKeyPress={handleKeyPress}>
         <WriteHeader>
           <div>
-          <BackButton onClick={backButtonHandler}>
-              <IconComponents
-                iconType='vectorLeft'
-                stroke ='#4C4C4C'
-              />
+            <BackButton onClick={backButtonHandler}>
+              <IconComponents iconType='vectorLeft' stroke='#4C4C4C' />
             </BackButton>
           </div>
           <div>앨범 수정하기</div>
@@ -229,30 +240,22 @@ function GroupWrite() {
             )}
           </WriteImageWrapper>
           <StDateWrapper>
-          <DivHeaderText>함께한 추억 기간 </DivHeaderText>
-          <Space
-      direction='vertical'
-      size={12}
-      style={{
-        width: '100%',
-      }}
-    >
-     <RangePicker
-      showTime={{}}
-      format='YYYY-MM-DD'
-      onChange={onChange}
-      style={{
-        width: '100%',
-        height: '44px',
-        backgroundColor: '#F5F5F5',
-        border: 'none',
-      }}
-      placeholder={[
-        moment(startDate).format('YYYY-MM-DD'), 
-        moment(endDate).format('YYYY-MM-DD')
-      ]}
-    />
-    </Space>
+            <DivHeaderText>함께한 추억 기간 </DivHeaderText>
+            <DateInput
+              value={startDate && endDate ? `${startDate.slice(0, 10)} ~ ${endDate.slice(0, 10)}` : ''}
+              onClick={() => setDateModal(!isDateModal)}
+              readOnly
+            />
+            {isDateModal && (
+              <DatePicker
+                ismodalopen={isDateModal}
+                onClose={() => setDateModal(false)}
+                startDate={startDate}
+                setStartDate={setStartDate}
+                endDate={endDate}
+                setEndDate={setEndDate}
+              />
+            )}
           </StDateWrapper>
           <PlaceContainer>
             <DivHeaderText>함께한 추억 장소</DivHeaderText>
@@ -287,7 +290,7 @@ function GroupWrite() {
             ))}
           </PlaceContainer>
           <div style={{ width: '100%' }}>
-          <DivHeaderText>함께한 친구들 </DivHeaderText>
+            <DivHeaderText>함께한 친구들 </DivHeaderText>
             <FriendSearchButton onClick={() => setModalOpen(!isModalOpen)}>
               <FriendContentWrap>
                 <FriendSearchImage
@@ -338,202 +341,3 @@ function GroupWrite() {
 }
 
 export default GroupWrite;
-
-const DivHeaderText = styled.p`
-  margin-top: 12px;
-  margin-bottom: 2px;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  color: #4C4C4C;
-`;
-
-const SelectFrindWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SelectFrindRemover = styled.button`
-  position: absolute;
-  border: none;
-  cursor: pointer;
-  width: 20px;
-  height: 20px;
-  border-radius: 100%;
-  color: #fff;
-  background-color: transparent;
-  top: 0;
-  right: 0;
-`;
-
-const ProfileImage = styled.img`
-  height: 60px;
-  width: 60px;
-  border-radius: 100%;
-`;
-
-const Form = styled.form`
-  width: 100%;
-`;
-
-const WriteHeader = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  height: 80px;
-  background-color: #fff;
-  align-items: center;
-`;
-
-const WriteBody = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 1rem;
-  overflow: scroll;
-`;
-
-const StDateWrapper = styled.div`
-  width: 100%;
-`;
-
-const GroupWriteInput = styled.input`
-  width: 100%;
-  height: 44px;
-  padding-right: 50px;
-  border-radius: 7px;
-  background-color: #f5f5f5;
-  border: none;
-
-  &:focus {
-    outline: none;
-  }
-`;
-const PlaceContainer = styled.div`
-  position: relative;
-  width: 100%;
-`;
-const PlaceInputWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  background-color: #f5f5f5;
-  border-radius: 7px;
-
-  .inputIcon {
-    margin-right: 8px;
-    margin-left: 10px;
-    margin-top: 7px;
-  }
-`;
-
-const SubmitButton = styled.button`
-  border: none;
-  background-color: transparent;
-  color: rgba(88, 115, 254, 1);
-  font-size: 16px;
-  font-weight: 500;
-  font-style: normal;
-  line-height: normal;
-  margin-right: 20px;
-`;
-
-const BackButton = styled.button`
-  margin-left: 20px;
-  background-color: transparent;
-  border: none;
-  position:relative;
-  top:5px
-`;
-
-const WriteImageWrapper = styled.div`
-  width: 100%;
-`;
-
-const ThumbedImage = styled.img`
-  width: 100%;
-  height: 20vh;
-  object-fit: cover;
-  border-radius: 7px;
-`;
-
-const ImageInput = styled.input`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  cursor: pointer;
-`;
-
-const ThumbnailWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 20vh;
-`;
-
-const PlaceResult = styled.div`
-  margin-left: 5px;
-  margin-top: 15px;
-  height: 30px;
-  display: inline-flex;
-  background-color: #5873fe;
-  border-radius: 20px;
-  color: #fff;
-  padding: 8px 4px 8px 10px;
-  font-size: 13px;
-  align-items: center;
-`;
-
-const PlaceRemoveButton = styled.button`
-  padding-top: 2px;
-  align-items: center;
-  border: none;
-  background-color: transparent;
-`;
-
-const PlaceAddButton = styled.button`
-  position: absolute;
-  top: 50%;
-  right: 10px;
-  transform: translateY(-50%);
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-// FriendSearch Button styledcomponent
-const FriendSearchButton = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 44px;
-  background-color: rgba(245, 245, 245, 1);
-`;
-
-const FriendSearchImage = styled.img`
-  width: 20px;
-  height: 20px;
-`;
-
-const FriendContentWrap = styled.div`
-  display: flex;
-  position: absolute;
-  left: 10px;
-  gap: 9px;
-`;
-
-const FriendSearchText = styled.p`
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  color: #c2c2c2;
-`;
--- src/pages/group/GroupWrite.jsx
+++ src/pages/group/GroupWrite.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { styled } from 'styled-components';
 import api from '../../api/index.jsx';
 import { uploadImage } from '../../api/uploadImage.js';
 import WriteImageUpload from '../../components/common/input/WriteImageUpload.jsx';
-import { DatePicker, Space } from 'antd';
+// import { DatePicker, Space } from 'antd';
 import Input from '../../components/common/input/Input.jsx';
 import FriendSearchModal from '../../components/common/modal/NicknameModal.jsx';
 import IconComponents from '../../components/common/iconComponent/IconComponents.jsx';
+import {
+  BackButton,
+  DivHeaderText,
+  FriendContentWrap,
+  FriendSearchButton,
+  FriendSearchImage,
+  FriendSearchText,
+  GroupWriteInput,
+  ImageInput,
+  PlaceAddButton,
+  PlaceContainer,
+  PlaceInputWrapper,
+  PlaceRemoveButton,
+  PlaceResult,
+  ProfileImage,
+  SelectFrindRemover,
+  SelectFrindWrapper,
+  StDateWrapper,
+  SubmitButton,
+  ThumbedImage,
+  ThumbnailWrapper,
+  WriteBody,
+  WriteHeader,
+  WriteImageWrapper,
+  Form,
+  DateInput,
+  DateInputWraper
+} from './styleContainer.js';
+import DatePicker from '../../components/common/modal/DatePicker.jsx';
 
 function GroupWrite() {
   const [groupName, setGroupName] = useState('');
@@ -15,24 +43,18 @@ function GroupWrite() {
   const [chosenFile, setChosenFile] = useState(null);
   const [place, setPlace] = useState('');
   const [places, setPlaces] = useState([]);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   const [participants, setParticipant] = useState([]);
   const [searchResult, setSearchResult] = useState([]);
   const [selectedFriends, setSelectedFriends] = useState([]);
 
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isDateModal, setDateModal] = useState(false);
 
-  const { RangePicker } = DatePicker;
-  const onChange = (value, dateString) => {
-    setStartDate(dateString[0]);
-    setEndDate(dateString[1]);
-  };
-
-  console.log('starDate', startDate);
-  console.log('endDate', endDate);
-
+  console.log('startdatewrite', startDate);
+  console.log('endDatewrite', endDate);
   const searchUser = async (nickname) => {
     try {
       const response = await api.get(`/nickname/${nickname}`, {
@@ -60,7 +82,7 @@ function GroupWrite() {
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-    }
+    } 
   };
 
   //데이터 보내는 로직
@@ -174,10 +196,7 @@ function GroupWrite() {
         <WriteHeader>
           <div>
             <BackButton onClick={backButtonHandler}>
-              <IconComponents
-                iconType='vectorLeft'
-                stroke ='#4C4C4C'
-              />
+              <IconComponents iconType='vectorLeft' stroke='#4C4C4C' />
             </BackButton>
           </div>
           <div>앨범 만들기</div>
@@ -208,7 +227,7 @@ function GroupWrite() {
                 height='20vh'
                 onImageChange={imageHandler}
                 bgcolor='rgba(245, 246, 248, 1)'
-                color="#BDBDBD"
+                color='#BDBDBD'
               >
                 썸네일 추가하기
               </WriteImageUpload>
@@ -216,25 +235,25 @@ function GroupWrite() {
           </WriteImageWrapper>
           <StDateWrapper>
             <DivHeaderText>함께한 추억 기간 </DivHeaderText>
-            <Space
-              direction='vertical'
-              size={12}
-              style={{
-                width: '100%',
-              }}
-            >
-              <RangePicker
-                showTime={{}}
-                format='YYYY-MM-DD'
-                onChange={onChange}
-                style={{
-                  width: '100%',
-                  height: '44px',
-                  backgroundColor: '#F5F5F5',
-                  border: 'none',
-                }}
+            <DateInputWraper>
+
+            <DateInput
+              value={startDate && endDate ? `${startDate} ~ ${endDate}` : ''}
+              onClick={() => setDateModal(!isDateModal)}
+              placeholder='2023.08.01 ~ 2023.08.03'
+              readOnly
+            />
+            </DateInputWraper>
+            {isDateModal && (
+              <DatePicker
+                ismodalopen={isDateModal}
+                onClose={() => setDateModal(false)}
+                startDate={startDate}
+                setStartDate={setStartDate}
+                endDate={endDate}
+                setEndDate={setEndDate}
               />
-            </Space>
+            )}
           </StDateWrapper>
           <PlaceContainer>
             <DivHeaderText>함께한 추억 장소</DivHeaderText>
@@ -294,7 +313,14 @@ function GroupWrite() {
               />
             )}
           </div>
-          <div style={{ display: 'flex', gap: '23px', marginTop:'14px', flexWrap:'wrap' }}>
+          <div
+            style={{
+              display: 'flex',
+              gap: '23px',
+              marginTop: '14px',
+              flexWrap: 'wrap',
+            }}
+          >
             {selectedFriends &&
               selectedFriends.map((item) => {
                 return (
@@ -320,208 +346,3 @@ function GroupWrite() {
 }
 
 export default GroupWrite;
-
-const DivHeaderText = styled.p`
-  margin-top: 12px;
-  margin-bottom: 2px;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 600;
-  color: #4c4c4c;
-`;
-
-const SelectFrindWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const SelectFrindRemover = styled.button`
-  position: absolute;
-  border: none;
-  cursor: pointer;
-  width: 20px;
-  height: 20px;
-  border-radius: 100%;
-  color: #fff;
-  background-color: transparent;
-  top: 0;
-  right: 0;
-`;
-
-const ProfileImage = styled.img`
-  height: 60px;
-  width: 60px;
-  border-radius: 100%;
-`;
-
-const Form = styled.form`
-  width: 100%;
-`;
-
-const WriteHeader = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  height: 80px;
-  background-color: #fff;
-  align-items: center;
-`;
-
-const WriteBody = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  padding: 1rem;
-`;
-
-const StDateWrapper = styled.div`
-  width: 100%;
-`;
-
-const GroupWriteInput = styled.input`
-  width: 100%;
-  height: 44px;
-  padding-right: 50px;
-  border-radius: 7px;
-  background-color: #f5f5f5;
-  border: none;
-
-  &:focus {
-    outline: none;
-  }
-  &::placeholder {
-    color: #c2c2c2;
-    font-size: 15px;
-    font-style: normal;
-    line-height: normal;
-  }
-`;
-const PlaceContainer = styled.div`
-  position: relative;
-  width: 100%;
-`;
-const PlaceInputWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  background-color: #f5f5f5;
-  border-radius: 7px;
-
-  .inputIcon {
-    margin-right: 8px;
-    margin-left: 10px;
-    margin-top: 7px;
-  }
-`;
-
-const SubmitButton = styled.button`
-  border: none;
-  background-color: transparent;
-  color: rgba(88, 115, 254, 1);
-  font-size: 16px;
-  font-weight: 500;
-  font-style: normal;
-  line-height: normal;
-  margin-right: 20px;
-`;
-
-const BackButton = styled.button`
-  margin-left: 20px;
-  background-color: transparent;
-  border: none;
-  position:relative;
-  top:5px
-`;
-
-const WriteImageWrapper = styled.div`
-  width: 100%;
-`;
-
-const ThumbedImage = styled.img`
-  width: 100%;
-  height: 20vh;
-  object-fit: cover;
-  border-radius: 7px;
-`;
-
-const ImageInput = styled.input`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: 0;
-  cursor: pointer;
-`;
-
-const ThumbnailWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 20vh;
-`;
-
-const PlaceResult = styled.div`
-  margin-left: 5px;
-  margin-top: 15px;
-  height: 30px;
-  display: inline-flex;
-  background-color: #5873fe;
-  border-radius: 20px;
-  color: #fff;
-  padding: 8px 4px 8px 10px;
-  font-size: 13px;
-  align-items: center;
-`;
-
-const PlaceRemoveButton = styled.button`
-  padding-top: 2px;
-  align-items: center;
-  border: none;
-  background-color: transparent;
-`;
-
-const PlaceAddButton = styled.button`
-  position: absolute;
-  top: 50%;
-  right: 10px;
-  transform: translateY(-50%);
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-// FriendSearch Button styledcomponent
-const FriendSearchButton = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 44px;
-  background-color: rgba(245, 245, 245, 1);
-`;
-
-const FriendSearchImage = styled.img`
-  width: 20px;
-  height: 20px;
-`;
-
-const FriendContentWrap = styled.div`
-  display: flex;
-  position: absolute;
-  left: 10px;
-  gap: 9px;
-`;
-
-const FriendSearchText = styled.p`
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  color: #c2c2c2;
-`;
-
