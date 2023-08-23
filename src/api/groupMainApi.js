b826134a4f4668ac283f5f2ee0314b22074d28d3
mmms2000 <moseskmcmmms@gmail.com>
2023-08-23 17:58:49 +0900

    fix: 주석

--- src/api/groupMainApi.js
+++ src/api/groupMainApi.js
@@ -7,8 +7,6 @@ const getGroupData = async () => {
     const groupData = response.data.findMyGroupData || [];
     const userId = response.data.userId;
 
-    // Incorporate user data into the group data.
-    // This depends on how you want to do it. Here's a simple example:
     if (userId) {
       localStorage.setItem('userId', userId);
     }
