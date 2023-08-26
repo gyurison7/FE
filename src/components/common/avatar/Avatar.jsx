00ae8fe106b366729b5bc209da9628d7f180f15d
metablog <khu10735@naver.com>
2023-08-27 00:41:24 +0900

    feat: 댓글 기능 구현

---
+++ src/components/common/avatar/Avatar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import PropTypes from 'prop-types';
+
+export default function Avatar({ width, height, src }) {
+  return <UserAvatar src={src} alt='avatar' width={width} height={height} />;
+}
+
+const UserAvatar = styled.img`
+  width: ${(props) => props.width || '40px'};
+  height: ${(props) => props.height || '40px'};
+  border-radius: 50%;
+`;
+Avatar.propTypes = {
+  src: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
--- src/pages/post/PostDetail.jsx
+++ src/pages/post/PostDetail.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../api/index.jsx';
 import { styled } from 'styled-components';
 import IconComponents from '../../components/common/iconComponent/IconComponents.jsx';
+import Comment from '../../components/common/comment/Comment.jsx';
+import Avatar from '../../components/common/avatar/Avatar.jsx';
 export default function PostDetail() {
   const { groupId, postId } = useParams();
   const [detail, setDetail] = useState(null);
-  console.log(detail?.memory);
+  console.log(detail);
+  const commentRef = useRef('');
+  console.log(detail);
   useEffect(() => {
     api
       .get(`/group/${groupId}/memory/${postId}`, { withCredentials: true })
@@ -14,6 +18,19 @@ export default function PostDetail() {
         setDetail(res.data);
       });
   }, []);
+  const commentSubmit = async (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const newComment = {
+        comment: commentRef.current.value,
+      };
+      api.post(`/group/${groupId}/memory/${postId}/comment`, newComment, {
+        withCredentials: true,
+      });
+
+      commentRef.current.value = '';
+    }
+  };
   return (
     <Wrap>
       <Head>
@@ -21,35 +38,64 @@ export default function PostDetail() {
         <p>게시물</p>
         <div></div>
       </Head>
-      <UserInfo>
-        <UserTitle>
-          <p>{detail?.memory.title} </p>
-          <div>...</div>
-        </UserTitle>
-        <UserInfoData>
-          <UserImg src={detail?.memory['User.profileUrl']} alt='avatar' />
-          <div>
-            <p>{detail?.memory['User.nickname']}</p>
+      <div style={{ paddingBottom: '75px' }}>
+        <UserInfo>
+          <UserTitle>
+            <p>{detail?.memory.title} </p>
+            <div>...</div>
+          </UserTitle>
+          <UserInfoData>
+            <Avatar
+              src={detail?.memory['User.profileUrl']}
+              width='40px'
+              height='40px'
+            />
             <div>
-              <p>2023.08.01</p>
+              <p>{detail?.memory['User.nickname']}</p>
+              <div>
+                <p>2023.08.01</p>
+              </div>
             </div>
+          </UserInfoData>
+        </UserInfo>
+        <IMG src={detail?.memory.imageUrl} alt='detaeil' />
+        <CommentWrap>
+          <div>
+            {detail?.comments.map((element) => {
+              return <Comment key={element.commentId} {...element} />;
+            })}
           </div>
-        </UserInfoData>
-      </UserInfo>
-      <IMG src={detail?.memory.imageUrl} alt='detaeil' />
+        </CommentWrap>
+      </div>
+      <Footer>
+        <Avatar src={detail?.user.profileUrl} width='40px' height='40px' />
+        <input
+          type='text'
+          placeholder='댓글을 달아주세요'
+          ref={commentRef}
+          onKeyPress={commentSubmit}
+        />
+      </Footer>
     </Wrap>
   );
 }
+const CommentWrap = styled.div`
+  width: 100%;
+`;
 const Wrap = styled.div`
+  position: relative;
   max-width: 428px;
-  border: 1px solid red;
+  height: 100vh;
 `;
 const Head = styled.div`
-  padding: 56px 24px 0px 25px;
+  padding: 56px 24px 18px 25px;
+  position: sticky;
+  top: 0;
   display: flex;
+  align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 11vh;
+  height: 93px;
   background: #5873fe;
   p {
     color: #fff;
@@ -61,11 +107,11 @@ const Head = styled.div`
 `;
 const IMG = styled.img`
   width: 100%;
-  height: 48vh;
+  height: 393px;
 `;
 const UserInfo = styled.div`
   width: 100%;
-  height: 102px;
+  height: 106px;
 `;
 const UserTitle = styled.div`
   display: flex;
@@ -93,8 +139,33 @@ const UserInfoData = styled.div`
     line-height: normal;
   }
 `;
-const UserImg = styled.img`
-  width: 35px;
-  height: 35px;
-  border-radius: 50%;
+
+const Footer = styled.div`
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  background-color: white;
+  gap: 15px;
+  padding: 19px 17px;
+  @media (max-width: 428px) {
+    width: 100%;
+  }
+  @media (min-width: 429px) {
+    width: 428px;
+  }
+  input {
+    width: 320px;
+    border-radius: 20px;
+    border: 0.1px solid #c5c5c7;
+    outline: none;
+
+    &::placeholder {
+      color: #c5c5c7;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      padding-left: 15px;
+    }
+  }
 `;
