863525ed4428d9c98b18a34a587b45403427e3e7
gyurison7 <orangetree401@gmail.com>
2023-08-28 12:16:31 +0900

    test

--- src/pages/kakao-login/KakaoLoginRedirect.jsx
+++ src/pages/kakao-login/KakaoLoginRedirect.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const KakaoLoginRedirect = () => {
-  return <div>Loading...</div>;
+  //const code = new URL(window.location.href).searchParams.get('code');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectToGroupMain = async () => {
+        try {
+            await navigate('/groupmain');
+            console.log("Navigation completed");
+        } catch (error) {
+            console.log("Navigation error:", error);
+        }
+    }
+
+    redirectToGroupMain();
+}, []);
+
 };
 
 export default KakaoLoginRedirect;
