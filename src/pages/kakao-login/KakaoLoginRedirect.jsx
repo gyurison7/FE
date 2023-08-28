7a117c746d79c1106c29dd593486059214770852
gyurison7 <orangetree401@gmail.com>
2023-08-28 12:33:06 +0900

    test

--- src/pages/kakao-login/KakaoLoginRedirect.jsx
+++ src/pages/kakao-login/KakaoLoginRedirect.jsx
@@ -1,23 +1,29 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import secureLocalStorage from 'react-secure-storage';
 
 const KakaoLoginRedirect = () => {
-  //const code = new URL(window.location.href).searchParams.get('code');
+  const code = new URL(window.location.href).searchParams.get('code');
   const navigate = useNavigate();
 
   useEffect(() => {
-    const redirectToGroupMain = async () => {
-        try {
-            await navigate('/groupmain');
-            console.log("Navigation completed");
-        } catch (error) {
-            console.log("Navigation error:", error);
+    const kakaoLogin = async () => {
+      try {
+        const response = await axios.get(
+          `https://honeyitem.shop/api/auth/login/kakao/callback?code=${code}`,
+          { withCredentials: true }
+        );
+        if (response.data) {
+          secureLocalStorage.setItem('userId', response.data.userId);
+          navigate('/groupmain');
         }
-    }
-
-    redirectToGroupMain();
-}, []);
-
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    kakaoLogin();
+  }, [code]);
 };
 
 export default KakaoLoginRedirect;
