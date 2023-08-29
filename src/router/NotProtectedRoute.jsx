01472ee28a46b2a61d9451ad4768123c02f2140a
gyurison7 <orangetree401@gmail.com>
2023-08-29 18:34:12 +0900

    fix: 로딩 제거

--- src/router/NotProtectedRoute.jsx
+++ src/router/NotProtectedRoute.jsx
@@ -1,33 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import api from '../api/index.jsx';
 
 function NotProtectedRoute() {
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const loginCheck = async () => {
-      setIsLoading(true);
       try {
         const response = await api.get('/auth/login', {
           withCredentials: true,
         });
-        if(response.data.success) {
+        if (response.data.success) {
           navigate('/groupmain');
         }
       } catch (error) {
         console.error(error);
-      } finally {
-        setIsLoading(false);
       }
     };
     loginCheck();
   }, [navigate]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return <Outlet />;
 }
--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -1,34 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import api from '../api/index.jsx';
 
 function ProtectedRoute() {
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const loginCheck = async () => {
-      setIsLoading(true);
       try {
         const response = await api.get('/auth/login', {
           withCredentials: true,
         });
-        if(!response.data.success) {
+        if (!response.data.success) {
           navigate('/login');
         }
       } catch (error) {
         console.error(error);
         navigate('/login');
-      } finally {
-        setIsLoading(false);
       }
     };
     loginCheck();
   }, [navigate]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   return <Outlet />;
 }
