6e7c4a6a24690be732710b75fda7406d7bb27497
gyurison7 <orangetree401@gmail.com>
2023-09-05 09:31:24 +0900

    feat: 시작페이지 carousel 구현

--- src/pages/app-introduction/Introduction.jsx
+++ src/pages/app-introduction/Introduction.jsx
@@ -1,230 +1,213 @@
-import React, { useRef, useState } from 'react';
-import { styled } from 'styled-components';
-import { Carousel } from 'antd';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import IconComponents from '../../components/common/iconComponent/IconComponents.jsx';
-import introduction from '../../assets/images/introduction.png';
+import { styled } from 'styled-components';
 import introduction1 from '../../assets/images/introduction1.png';
 import introduction2 from '../../assets/images/introduction2.png';
 import introduction3 from '../../assets/images/introduction3.png';
 import introduction4 from '../../assets/images/introduction4.png';
-
-const slideData = [
-  {
-    skipText: '건너뛰기',
-    content: (
-      <>
-        친구들끼리 <br /> 추억 앨범을 만들어보세요
-      </>
-    ),
-    imageSrc: introduction,
-  },
-  {
-    skipText: '건너뛰기',
-    content: (
-      <>
-        함께 공유하고 싶은 <br />
-        순간들을 올려보세요
-      </>
-    ),
-    imageSrc: introduction1,
-  },
-  {
-    skipText: '건너뛰기',
-    content: (
-      <>
-        친구들과의 소통도 <br /> 할 수 있어요
-      </>
-    ),
-    imageSrc: introduction2,
-  },
-  {
-    skipText: '건너뛰기',
-    content: (
-      <>
-        날짜 검색으로 <br /> 이전 추억들을 돌아봐요
-      </>
-    ),
-    imageSrc: introduction3,
-  },
-];
+import introduction5 from '../../assets/images/introduction5.png';
+import Button from '../../components/common/button/Button.jsx';
 
 function Introduction() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [startX, setStartX] = useState(null);
   const navigate = useNavigate();
-  const carouselRef = useRef(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
 
-  const onClickHandler = () => {
-    if (currentSlide < 4) {
-      navigate('/login');
-    } else {
-      carouselRef.current.next();
+  const images = [
+    introduction1,
+    introduction2,
+    introduction3,
+    introduction4,
+    introduction5,
+  ];
+  const texts = [
+    '친구들끼리<br/>추억 앨범을 만들어보세요',
+    '함께 공유하고 싶은<br/>순간들을 올려보세요',
+    '친구들과의 소통도<br/>할 수 있어요',
+    '날짜 검색으로<br/>이전 추억들을 돌아봐요',
+    '지금 바로<br/>메모리밍글을 시작해보세요!',
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images]);
+
+  const handleTouchStart = (e) => {
+    e.preventDefault();
+    // 모바일 환경
+    if (e.type === 'touchstart') {
+      setStartX(e.touches[0].clientX);
+    }
+    // 데스크탑 환경
+    else if (e.type === 'mousedown') {
+      setStartX(e.clientX);
     }
   };
-  const onClickHandlerLogin = () => {
-    navigate('/login');
+
+  const handleTouchEnd = (e) => {
+    let endX;
+    // 모바일 환경
+    if (e.type === 'touchend') {
+      endX = e.changedTouches[0].clientX;
+    }
+    // 데스크탑 환경
+    else if (e.type === 'mouseup') {
+      endX = e.clientX;
+    }
+
+    if (startX - endX > 50) {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    } else if (endX - startX > 50) {
+      setCurrentIndex(
+        (prevIndex) => (prevIndex - 1 + images.length) % images.length
+      );
+    }
   };
 
-  const goToNextSlide = () => {
-    carouselRef.current.next();
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  const goToPrevSlide = () => {
-    carouselRef.current.prev();
-  };
-
-  const afterSlideChange = (current) => {
-    setCurrentSlide(current);
+  const renderText = (text) => {
+    return text.split('<br/>').map((part, index) => (
+      <React.Fragment key={index}>
+        {part}
+        {index !== text.split('<br/>').length - 1 && <br />}
+      </React.Fragment>
+    ));
   };
 
   return (
-    <Wrap>
-      <CarouselWrap>
-        <Carousel ref={carouselRef} dotPosition='top' afterChange={afterSlideChange}>
-          {slideData.map((slide, index) => (
-            <div key={index}>
-              {currentSlide === index && (
-                <Skip onClick={onClickHandler}>{slide.skipText}</Skip>
-              )}
-              <Description
-                style={{
-                  height: '30vh',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                {slide.content}
-              </Description>
-              <CustomImage src={slide.imageSrc} alt='introduction' />
-            </div>
-          ))}
-          <WrapButton>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
+    <Wrapper>
+      <Dots>
+        {images.map((_, index) => (
+          <Dot key={index} active={currentIndex === index} />
+        ))}
+      </Dots>
+      {currentIndex !== images.length - 1 && (
+        <ButtonContainer>
+          <button onClick={() => navigate('/login')}>건너뛰기</button>
+          <button onClick={handleNext}>다음</button>
+        </ButtonContainer>
+      )}
+      <Slider
+        onTouchStart={handleTouchStart}
+        onMouseDown={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        onMouseUp={handleTouchEnd}
+      >
+        <H1>{renderText(texts[currentIndex])}</H1>
+        <img
+          src={images[currentIndex]}
+          alt={`slide ${currentIndex}`}
+          className={currentIndex === images.length - 1 ? 'last' : ''}
+        />
+        {currentIndex === images.length - 1 && (
+          <StartButtonContainer>
+            <Button
+              type='button'
+              size='large'
+              background='#FFF'
+              color='#5873FE'
+              onClick={() => navigate('/login')}
             >
-              <Description
-                style={{
-                  height: '30vh',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  color: '#FFF',
-                }}
-              >
-                지금 바로 <br /> 메모리밍글을 시작해보세요
-              </Description>
-              <CustomImageLast src={introduction4} alt='introduction4' />
-              <div
-                style={{
-                  height: '20vh',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Button onClick={onClickHandlerLogin}>시작하기</Button>
-              </div>
-            </div>
-          </WrapButton>
-        </Carousel>
-        <PrevNext>
-          <IconComponents
-            iconType='vectorLeft'
-            stroke='white'
-            onClick={goToPrevSlide}
-          />
-          {currentSlide <= slideData.length - 1 && (
-            <IconComponents
-              iconType='vectorRight'
-              stroke='white'
-              viewBox='0 0 1 20'
-              onClick={goToNextSlide}
-            />
-          )}
-        </PrevNext>
-      </CarouselWrap>
-    </Wrap>
+              시작하기
+            </Button>
+          </StartButtonContainer>
+        )}
+      </Slider>
+    </Wrapper>
   );
 }
 
 export default Introduction;
 
-const Wrap = styled.div`
-  background: #5873fe;
-  max-width: 428px;
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
   height: 100vh;
-`;
-const Skip = styled.p`
-  padding-left: 10px;
-  top: 6px;
-  color: #fff;
-  cursor: pointer;
-  position: fixed;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 129.336%;
+  max-width: 428px;
+  display: flex;
+  justify-content: center;
+  background-color: #5873fe;
+  overflow: hidden;
 `;
 
-const PrevNext = styled.div`
-  width: 100%;
-  position: fixed;
-  top: 50%;
-  padding: 0 10px;
+const Dots = styled.div`
+  position: absolute;
+  top: 4%;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  gap: 10px;
+`;
+
+const Dot = styled.span`
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: ${(props) => (props.active ? '#FFF' : '#A5B3FF')};
+  transition: background-color 0.3s;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  top: 3.5%;
+  left: 0;
+  right: 0;
   display: flex;
   justify-content: space-between;
-  @media (max-width: 428px) {
-    width: 100%;
-  }
-  @media (min-width: 429px) {
-    width: 428px;
-  }
-`;
-const Description = styled.p`
-  color: #fff;
-  text-align: center;
-  font-size: 32px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-`;
-const CustomImage = styled.img`
-  width: 70%;
-  margin: 0 auto;
-  height: 70vh;
-`;
-const CustomImageLast = styled(CustomImage)`
-  width: 50%;
-  height: 30vh;
-`;
-const CarouselWrap = styled.div`
   width: 100%;
+  padding: 0 1em;
+
+  button {
+    background-color: transparent;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+  }
 `;
-const WrapButton = styled.div`
-  height: 100vh;
+
+const Slider = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  margin-bottom: -2vh;
+
+  img {
+    width: 80%;
+    &.last {
+      width: 267px;
+      height: 261px;
+      margin-top: -19vh;
+    }
+  }
 `;
-const Button = styled.span`
-  position: sticky;
-  border-radius: 28px;
-  bottom: -100px;
-  cursor: pointer;
-  font-weight: 600;
-  color: #5873fe;
-  background: #fff;
-  height: 57px;
+
+const H1 = styled.h1`
+  margin-top: 15vh;
+  text-align: center;
+  font-size: 30px;
+  color: #fff;
+  font-weight: 700;
+`;
+
+const StartButtonContainer = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  @media (max-width: 428px) {
-    width: 342px;
-    margin: 0 auto;
-  }
-  @media (min-width: 429px) {
-    width: 342px;
-    margin: 0 auto;
+  position: relative;
+  bottom: 25vh;
+
+  button {
+    width: 100%;
+    font-size: 16px;
+    font-weight: 700;
   }
 `;
