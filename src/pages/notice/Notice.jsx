beab4ff44ddb110959815bc6a0fb0f996759b20f
gyurison7 <orangetree401@gmail.com>
2023-09-07 05:46:28 +0900

    test 원복

--- src/pages/notice/Notice.jsx
+++ src/pages/notice/Notice.jsx
@@ -1,42 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { styled } from 'styled-components';
 import Header from '../../components/common/header/Header.jsx';
 import Footer from '../../layout/footer/Footer.js';
-import socketIoClient from 'socket.io-client';
-import axios from 'axios';
 
 const Notice = () => {
-  const userId = localStorage.getItem('userId');
-  console.log(userId);
-
-  useEffect(() => {
-    const socket = socketIoClient('https://api.memorymingle.shop');
-    
-    socket.emit('register', userId);
-
-    socket.on('newUserAdded', (data) => {
-      console.log('New user added:', data.userId);
-      // 여기서 알림 기능을 구현할 수 있습니다.
-    });
-
-    // 컴포넌트가 unmount 될 때 소켓 연결 종료
-    return () => {
-      socket.disconnect();
-    };
-  }, [userId]);
-
-  const buttonClickHandler = async () => {
-    const groupId = 1;
-    try {
-      const response = await axios.post(
-        `https://api.memorymingle.shop/group/${groupId}`
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <Wrapper>
       <Header title='알림' />
@@ -45,7 +12,6 @@ const Notice = () => {
         <button>지난 알림</button>
       </Navbar>
       <NoticeContainer>
-        <button onClick={buttonClickHandler}>알림 테스트</button>
         <div>
           <p>앨범 `찐친즈모임`에 초대되셨습니다.</p>
           <p>날짜</p>
