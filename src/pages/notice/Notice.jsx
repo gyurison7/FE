59ff7e74fc0dd8ce569418b20edc175e6f58644b
gyurison7 <orangetree401@gmail.com>
2023-09-13 15:16:53 +0900

    feat: 알림 기능 구현(작업중)

--- src/pages/notice/Notice.jsx
+++ src/pages/notice/Notice.jsx
@@ -1,103 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { css, styled } from 'styled-components';
 import Header from '../../components/common/header/Header.jsx';
 import Footer from '../../layout/footer/Footer.js';
-import socketIoClient from 'socket.io-client';
 import api from '../../api/index.jsx';
-import { useToast } from '../../hooks/useToast.jsx';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko'; // 한국어 로케일 추가
+import relativeTime from 'dayjs/plugin/relativeTime'; // relativeTime 플러그인 추가
+
+dayjs.extend(relativeTime); // relativeTime 플러그인 활성화
+dayjs.locale('ko');
 
 const Notice = () => {
-  const [socketConnection, setSocketConnection] = useState();
   const [activeNav, setActiveNav] = useState('new');
-  const loginUserId = localStorage.getItem('userId');
-
-  console.log('loginUserId', loginUserId);
-
-  const { showToast } = useToast();
-
-  useEffect(() => {
-    const ENDPOINT = 'https://api.memorymingle.shop';
-    //const ENDPOINT = 'http://localhost:4000';
-
-    const socket = socketIoClient(ENDPOINT, {
-      withCredentials: true,
-    });
-
-    socket.on('connect', () => {
-      console.log('연결 상태', socket.connected); // 이곳에서 true가 출력되어야 함
-
-      socket.on('connect_error', (error) => {
-        console.log('Connection Error:', error);
-      });
-
-      socket.emit('register', loginUserId);
-
-      socket.on('newUserAdded', (data) => {
-        console.log('이벤트 발생');
-        console.log(data);
-        const thumbnailUrl = data.thumbnailUrl;
-        const groupName = data.groupName;
-        const groupId = data.groupId;
-        const userId = data.userId;
-
-        console.log(
-          `유처 추가 확인:${userId} ${thumbnailUrl},${groupId}번 ${groupName}에 초대되셨습니다.`
-        );
-        showToast(`${groupName}에 초대되셨습니다.`);
-        // 여기서 알림 기능을 구현할 수 있습니다.
-        // const sendNotification = async () => {
-        //   try {
-        //     const response = await api.post('/notification', {
-        //       thumbnailUrl,
-        //       message: `${groupName}에 초대되셨습니다.`,
-        //       groupId,
-        //       userId,
-        //     });
-        //     console.log(response);
-        //   } catch (error) {
-        //     console.log(error);
-        //   }
-        // };
-        // sendNotification();
-      });
-
-      setSocketConnection(socket);
-    });
-
-    // 컴포넌트가 unmount 될 때 소켓 연결 종료
-    return () => {
-      if (socket) {
-        socket.disconnect();
-      }
-    };
-  }, [loginUserId]);
+  const [noticeList, setNoticeList] = useState([]);
+  const navigate = useNavigate();
 
   const noticeClickHandler = (name) => {
     setActiveNav(name);
   };
 
-  const buttonClickHandler = async () => {
-    const groupId = 2;
-    try {
-      const response = await api.post(
-        `/group/${groupId}`,
-        { userId: 15, thumbnailUrl: '11', groupName: '테스트' },
-        {
-          withCredentials: true,
-        }
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const disconnectSocket = () => {
-    if (socketConnection) {
-      socketConnection.disconnect();
-      setSocketConnection(null); // 상태를 null로 설정하여 연결을 끊었다는 것을 표시
-    }
-  };
+  useEffect(() => {
+    const getNotification = async () => {
+      try {
+        const response = await api.get('/notification');
+        console.log(response.data.data);
+        setNoticeList(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getNotification();
+  }, []);
 
   return (
     <Wrapper>
@@ -117,12 +51,23 @@ const Notice = () => {
         </button>
       </Navbar>
       <NoticeContainer>
-        <button onClick={buttonClickHandler}>알림 테스트</button>
-        <button onClick={disconnectSocket}>연결 끊기</button>
-        <div>
-          <p>앨범 `찐친즈모임`에 초대되셨습니다.</p>
-          <p>날짜</p>
-        </div>
+        {noticeList
+          .slice()
+          .reverse()
+          .map((notice) => {
+            const relativeDate = dayjs(notice['Participants.createdAt']).fromNow();
+            return (
+              <NoticeItem key={notice['Participants.participantid']}>
+                <img src={notice.thumbnailUrl} alt='썸네일' />
+                <button onClick={() => navigate(`/postmain/${notice.groupId}`)}>
+                  <p className='message'>
+                    앨범 {notice.groupName}에 초대되셨습니다.
+                  </p>
+                  <p className='date'>{relativeDate}</p>
+                </button>
+              </NoticeItem>
+            );
+          })}
       </NoticeContainer>
       <Foot>
         <Footer />
@@ -132,6 +77,12 @@ const Notice = () => {
 };
 
 export default Notice;
+
+const buttonStyle = css`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -150,9 +101,8 @@ const Navbar = styled.div`
   margin: 30px 0 0 24px;
 
   button {
+    ${buttonStyle};
     color: #c2c2c2;
-    background-color: transparent;
-    border: none;
     font-size: 16px;
     font-weight: 600;
   }
@@ -166,6 +116,34 @@ const Navbar = styled.div`
 
 const NoticeContainer = styled.div`
   margin: 18px 0 0 24px;
+`;
+
+const NoticeItem = styled.div`
+  display: flex;
+  gap: 9px;
+  margin-bottom: 1vh;
+
+  img {
+    width: 49px;
+    height: 49px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  button {
+    ${buttonStyle};
+    font-size: 14px;
+    text-align: left;
+
+    .message {
+      color: #4c4c4c;
+      font-weight: 600;
+    }
+
+    .date {
+      color: #a6a6a6;
+    }
+  }
 `;
 
 const Foot = styled.div`
